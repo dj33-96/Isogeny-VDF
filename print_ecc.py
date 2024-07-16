91be3e23de94475f12ec00156b069a4392e5475f
dj33-96 <dj.davidjacquemin@gmail.com>
2024-07-16 15:52:09 +0200

    Update print_ecc.py

--- print_ecc.py
+++ print_ecc.py
@@ -32,6 +32,7 @@ P_array = [574448099311,
            501974515280983173562892287,
            1658539334852043956605014686969369842243820155059458240864380460354175875596746126442552006529285980003318752448184629099761975446397870870332614114924526019655624366944770281974501212314250998405682106067115619475132937730960746637418716661215852316737808364060021400361715167852784987427099666051667608448888314571788638487985846716927693574019769274326804364407638203115258648742883949562283207610572974523311143132532016594886767069744238342663307263]
 
+
 Mem_num = 128 #Number of memory register
 Num = int(log2(Mem_num))
 W = W_array[index] # prime size
@@ -48,12 +49,24 @@ elif ((x)*p)%N == 1:
 
 print("check p_prime:",(-p_prime)*p%N==1)
 
+# ------------------------------------------------------
+#For EC function test bench
+#boolean to set to true to generate random value.
+rand_dbl,rand_4isoc,rand_4isoe = True,True,True
+#Input data for the testbench (set rand to false to use this)
+#Manually enter the data here
+in_dbl = [randint(0,p),randint(0,p),8,4]
+in_4isoc = [randint(0,p),randint(0,p)]
+in_4isoe = [randint(0,p),randint(0,p),randint(0,p),randint(0,p),randint(0,p)]
+
+
 # ----- create directory
 dir_name = "cryptopro_{}x{}/".format(W,W)
 if not(os.path.exists(dir_name)):
     os.mkdir(dir_name)
 
 # ----- print ecc functions
-print_dbl_tb(W,p,Num,correction,0,dir_name)
-print_iso_4_c_tb(W,p,Num,correction,dir_name)
-print_iso_4_e_tb(W,p,Num,correction,dir_name)
+#Standard E(A): y**2 = x**3 + 6*x + x curve
+print_dbl_tb(W,p,Num,correction,rand_dbl,in_dbl,dir_name)
+print_iso_4_c_tb(W,p,Num,correction,rand_4isoc,in_4isoc,dir_name)
+print_iso_4_e_tb(W,p,Num,correction,rand_4isoe,in_4isoe,dir_name)
